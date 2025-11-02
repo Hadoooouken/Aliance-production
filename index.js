978164e628d20cb5c76431cedded7b18639a4df8
Filipp <wolverinexdd@yandex.ru>
2025-11-02 20:44:58 +0400

    fix navbar after scroll. added sprite - remove svg

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const navbar = document.querySelector('.header__navbar')
-console.log();
+const logo = document.querySelector('.logo__svg use')
+
 
 window.addEventListener('scroll', () => {
     if (this.scrollY > 1) {
         navbar.classList.add('header__navbar--light')
-        navbar.style='borderbottom'
+        logo.href.baseVal = '/images/sprites.svg#logo-dark'
     }
 
     else {
         navbar.classList.remove('header__navbar--light')
+        logo.href.baseVal = '/images/sprites.svg#logo-light'
         console.log('конец');
     }
 });
