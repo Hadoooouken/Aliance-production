4abdac2e988b007d77d91bb3127b506884e9bc99
Filipp <wolverinexdd@yandex.ru>
2025-11-02 19:04:42 +0400

    fix: navbar scroll

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const navbar = document.querySelector('.header__navbar')
+console.log();
 
 window.addEventListener('scroll', () => {
-        if (this.scrollY > 1) {
-            navbar.classList.add('header__navbar--light')
-        }
+    if (this.scrollY > 1) {
+        navbar.classList.add('header__navbar--light')
+        navbar.style='borderbottom'
+    }
 
-        else {
-            navbar.classList.remove('header__navbar--light')
-            console.log('конец');
-        }
-    // console.log(navbar.style.backgroundColor = '#fff');
-
+    else {
+        navbar.classList.remove('header__navbar--light')
+        console.log('конец');
+    }
 });
+
+
+
+
