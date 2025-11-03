f02dc2b882369913b03ddbfd59d44fa2b64a2036
Filipp <wolverinexdd@yandex.ru>
2025-11-03 18:28:04 +0400

    change img path on index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const logo = document.querySelector('.logo__svg use')
 window.addEventListener('scroll', () => {
     if (this.scrollY > 1) {
         navbar.classList.add('header__navbar--light')
-        logo.href.baseVal = '/images/sprites.svg#logo-dark'
+        logo.href.baseVal = './images/sprites.svg#logo-dark'
     }
 
     else {
         navbar.classList.remove('header__navbar--light')
-        logo.href.baseVal = '/images/sprites.svg#logo-light'
+        logo.href.baseVal = './images/sprites.svg#logo-light'
         console.log('конец');
     }
 });
