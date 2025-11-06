967902c93828e131091a773bc1688dc297269733
Filipp <wolverinexdd@yandex.ru>
2025-11-06 23:42:46 +0400

    added mobile menu and mobile adaptive layout

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const navbar = document.querySelector('.header__navbar')
 const logo = document.querySelector('.logo__svg use')
-
+const burgerButton = document.querySelector('[data-js-header-burger-button]')
+const mobileOverlay = document.querySelector('[data-js-header-overlay]')
+console.log(burgerButton);
+console.log(mobileOverlay);
 
 window.addEventListener('scroll', () => {
     if (this.scrollY > 1) {
@@ -14,6 +17,11 @@ window.addEventListener('scroll', () => {
         console.log('конец');
     }
 });
+
+burgerButton.addEventListener('click', () => {
+    mobileOverlay.classList.toggle('is-open')
+    console.log('click');
+})
 
 
 
