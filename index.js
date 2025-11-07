31b3c4ece074a20cb32f92937f699ddc78d81c3c
Filipp <wolverinexdd@yandex.ru>
2025-11-08 01:16:39 +0400

    fix mobile menu add src

--- index.js
+++ index.js
@@ -2,26 +2,92 @@ const navbar = document.querySelector('.header__navbar')
 const logo = document.querySelector('.logo__svg use')
 const burgerButton = document.querySelector('[data-js-header-burger-button]')
 const mobileOverlay = document.querySelector('[data-js-header-overlay]')
-console.log(burgerButton);
-console.log(mobileOverlay);
+
+
+const lightModeOn = () => {
+    navbar.classList.add('header__navbar--light')
+    logo.href.baseVal = './images/sprites.svg#logo-dark'
+}
+
+
+
+const lightModeOff = () => {
+    navbar.classList.remove('header__navbar--light')
+    logo.href.baseVal = './images/sprites.svg#logo-light'
+}
+
+const openMenu = () => {
+    document.body.classList.add('is-lock')
+    mobileOverlay.classList.add('is-open')
+    burgerButton.classList.add('close-menu')
+    lightModeOn()
+}
+
+const closeMenu = () => {
+    document.body.classList.remove('is-lock')
+    mobileOverlay.classList.remove('is-open')
+     burgerButton.classList.remove('close-menu')
+    lightModeOff()
+}
 
 window.addEventListener('scroll', () => {
-    if (this.scrollY > 1) {
-        navbar.classList.add('header__navbar--light')
-        logo.href.baseVal = './images/sprites.svg#logo-dark'
-    }
-
-    else {
-        navbar.classList.remove('header__navbar--light')
-        logo.href.baseVal = './images/sprites.svg#logo-light'
-        console.log('конец');
-    }
+    this.scrollY > 1 ? lightModeOn() : lightModeOff()
 });
 
 burgerButton.addEventListener('click', () => {
-    mobileOverlay.classList.toggle('is-open')
-    console.log('click');
+    mobileOverlay.classList.contains('is-open') ? closeMenu() : openMenu()
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
