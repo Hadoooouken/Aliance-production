a3311d65f9f2be49943c6e380f2f948dea3bf6a4
Filipp <wolverinexdd@yandex.ru>
2025-12-29 00:28:20 +0400

    fix scroll, added about page

--- src/index.js
+++ src/index.js
@@ -1,87 +1,87 @@
-const navbar = document.querySelector('.header__navbar')
-const logoLight = document.querySelector('.logo__svg--light')
-const logoDark = document.querySelector('.logo__svg--dark')
+// const navbar = document.querySelector('.header__navbar')
+// const logoLight = document.querySelector('.logo__svg--light')
+// const logoDark = document.querySelector('.logo__svg--dark')
 
-const burgerButton = document.querySelector('[data-js-header-burger-button]')
-const mobileOverlay = document.querySelector('[data-js-header-overlay]')
-
-
-
-const modal = document.querySelector('.modal')
-const openModalButtons = document.querySelectorAll('[data-toggle="modal"]')
-const modalContent = modal.querySelector('.modal__dialog')
-
-document.addEventListener("click", (event) => {
-    if (
-        event.target.dataset.toggle === "modal" ||
-        event.target.parentNode.dataset.toggle === "modal" ||
-        (!event.composedPath().includes(modalContent) && modal.classList.contains("is-open"))
-    ) {
-        event.preventDefault();
-        modal.classList.toggle("is-open");
-    }
-})
+// const burgerButton = document.querySelector('[data-js-header-burger-button]')
+// const mobileOverlay = document.querySelector('[data-js-header-overlay]')
 
 
-const closeModalByEsc = (evt) => {
+// // const modal = document.querySelector('.modal')
+// // const openModalButtons = document.querySelectorAll('[data-toggle="modal"]')
+// // const modalContent = modal.querySelector('.modal__dialog')
 
-    if (evt.key === 'Escape' && modal.classList.contains('is-open')) {
-        modal.classList.toggle("is-open");
-    }
-}
+// document.addEventListener("click", (event) => {
+//     if(!modalContent) return
+//     if (
+//         event.target.dataset.toggle === "modal" ||
+//         event.target.parentNode.dataset.toggle === "modal" ||
+//         (!event.composedPath().includes(modalContent) && modal.classList.contains("is-open"))
+//     ) {
+//         event.preventDefault();
+//         modal.classList.toggle("is-open");
+//     }
+// })
 
-document.addEventListener('keydown', closeModalByEsc)
+
+// const closeModalByEsc = (evt) => {
+
+//     if (evt.key === 'Escape' && modal.classList.contains('is-open')) {
+//         modal.classList.toggle("is-open");
+//     }
+// }
+
+// document.addEventListener('keydown', closeModalByEsc)
 
 
-const lightModeOn = () => {
-    navbar.classList.add('header__navbar--light')
-    logoLight.style.display = 'none'
-    logoDark.style.display = "block"
-}
+// // const lightModeOn = () => {
+// //     navbar.classList.add('header__navbar--light')
+// //     logoLight.style.display = 'none'
+// //     logoDark.style.display = "block"
+// // }
 
-const lightModeOff = () => {
-    navbar.classList.remove('header__navbar--light')
-    logoLight.style.display = 'block'
-    logoDark.style.display = "none"
-}
+// // const lightModeOff = () => {
+// //     navbar.classList.remove('header__navbar--light')
+// //     logoLight.style.display = 'block'
+// //     logoDark.style.display = "none"
+// // }
 
-const openMenu = () => {
-    document.body.classList.add('is-lock')
-    mobileOverlay.classList.add('is-open')
-    burgerButton.classList.add('close-menu')
-    lightModeOn()
-}
+// const openMenu = () => {
+//     document.body.classList.add('is-lock')
+//     mobileOverlay.classList.add('is-open')
+//     burgerButton.classList.add('close-menu')
+//     lightModeOn()
+// }
 
-const closeMenu = () => {
-    document.body.classList.remove('is-lock')
-    mobileOverlay.classList.remove('is-open')
-    burgerButton.classList.remove('close-menu')
-    lightModeOff()
-}
+// const closeMenu = () => {
+//     document.body.classList.remove('is-lock')
+//     mobileOverlay.classList.remove('is-open')
+//     burgerButton.classList.remove('close-menu')
+//     lightModeOff()
+// }
 
-window.addEventListener('resize', () => {
-    if (window.innerWidth > 992 && mobileOverlay.classList.contains('is-open')) {
-        closeMenu()
-    }
-});
+// window.addEventListener('resize', () => {
+//     if (window.innerWidth > 992 && mobileOverlay.classList.contains('is-open')) {
+//         closeMenu()
+//     }
+// });
 
-const isPageScrolled = () => {
-    return window.scrollY > 1
-}
+// const isPageScrolled = () => {
+//     return window.scrollY > 1
+// }
 
-window.addEventListener('scroll', () => {
-    isPageScrolled() ? lightModeOn() : lightModeOff()
-});
+// window.addEventListener('scroll', () => {
+//     isPageScrolled() ? lightModeOn() : lightModeOff()
+// });
 
-burgerButton.addEventListener('click', () => {
-    if (mobileOverlay.classList.contains('is-open')) {
-        closeMenu()
-        isPageScrolled() ? lightModeOn() : lightModeOff()
-    }
-    else {
-        openMenu()
-    }
-})
+// burgerButton.addEventListener('click', () => {
+//     if (mobileOverlay.classList.contains('is-open')) {
+//         closeMenu()
+//         isPageScrolled() ? lightModeOn() : lightModeOff()
+//     }
+//     else {
+//         openMenu()
+//     }
+// })
 
 
 const swiper = new Swiper('.swiper', {
@@ -161,52 +161,141 @@ const swiperBlog = new Swiper('.swiper__blog', {
 
 
 
-// const modal = document.querySelector('.modal')
-// const openModalButtons = document.querySelectorAll('[data-js-header-modal-button]')
-// const closeModalButton = modal.querySelector('[data-js-header-close-modal-button]')
-// const modalContent = modal.querySelector('.modal__dialog')
 
 
 
-// const openModal = (modal) => {
-//     document.body.classList.add('is-lock')
-//     modal.classList.add('is-open')
-// }
 
-// const closeModal = (modal) => {
-//     document.body.classList.remove('is-lock')
-//     modal.classList.remove('is-open')
 
-// }
 
-// openModalButtons.forEach((button) => {
-//     button.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         openModal(modal)
-//     })
-// })
 
-// closeModalButton.addEventListener('click', () => {
-//     closeModal(modal)
 
-// })
 
-// const closeModalByOverlay = (e) => {
-//     if (e.target === modal) {
-//         closeModal(e.target)
-//     }
-// }
 
-// modal.addEventListener('click', closeModalByOverlay)
 
-// const closeModalByEsc = (evt) => {
 
-//     if (evt.key === 'Escape' && modal.classList.contains('is-open')) {
-//         closeModal(modal)
-//     }
-// }
 
-// window.addEventListener('keydown', closeModalByEsc)
+
+
+const modal = document.querySelector('.modal')
+const openModalButtons = document.querySelectorAll('[data-js-header-modal-button]')
+const closeModalButton = modal.querySelector('[data-js-header-close-modal-button]')
+const modalContent = modal.querySelector('.modal__dialog')
+
+
+
+const navbar = document.querySelector('.header__navbar')
+const logoLight = document.querySelector('.logo__svg--light')
+const logoDark = document.querySelector('.logo__svg--dark')
+const ismainPage = document.body.classList.contains('main-page')
+
+
+const burgerButton = document.querySelector('[data-js-header-burger-button]')
+const mobileOverlay = document.querySelector('[data-js-header-overlay]')
+
+
+const changeNavbarHeight = (height) => {
+    navbar.style.height = height
+    console.log(height);
+}
+
+const closeModalByEsc = (evt) => {
+
+    if (evt.key === 'Escape' && modal.classList.contains('is-open')) {
+        modal.classList.toggle("is-open");
+    }
+}
+
+const lightModeOn = () => {
+    navbar.classList.add('header__navbar--light')
+
+}
+
+const lightModeOff = () => {
+    navbar.classList.remove('header__navbar--light')
+
+}
+
+const openMenu = () => {
+    document.body.classList.add('is-lock')
+    mobileOverlay.classList.add('is-open')
+    burgerButton.classList.add('close-menu')
+    lightModeOn()
+}
+
+const closeMenu = () => {
+    document.body.classList.remove('is-lock')
+    mobileOverlay.classList.remove('is-open')
+    burgerButton.classList.remove('close-menu')
+    lightModeOff()
+}
+
+
+
+
+
+document.addEventListener('keydown', closeModalByEsc)
+
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 992 && mobileOverlay.classList.contains('is-open')) {
+        closeMenu()
+    }
+});
+
+
+window.addEventListener('scroll', () => {
+    window.scrollY > 1 ? changeNavbarHeight(`4.75rem`) : changeNavbarHeight('5.72rem') 
+    if(ismainPage) {
+        window.scrollY > 1  ? lightModeOn() : lightModeOff()
+    } 
+  
+});
+
+document.addEventListener("click", (event) => {
+    if (!modalContent) return
+    if (
+        event.target.dataset.toggle === "modal" ||
+        event.target.parentNode.dataset.toggle === "modal" ||
+        (!event.composedPath().includes(modalContent) && modal.classList.contains("is-open"))
+    ) {
+        event.preventDefault();
+        modal.classList.toggle("is-open");
+    }
+})
+
+
+burgerButton.addEventListener('click', () => {
+    if (mobileOverlay.classList.contains('is-open')) {
+        closeMenu()
+        window.scrollY > 1  ? lightModeOn() : lightModeOff()
+    }
+    else {
+        openMenu()
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
