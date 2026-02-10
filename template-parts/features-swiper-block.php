a82317fddf786a992ed7cc20db3d5cdab58ad0ec
Filipp <wolverinexdd@yandex.ru>
2026-02-10 13:31:39 +0400

    fix color features slider

--- template-parts/features-swiper-block.php
+++ template-parts/features-swiper-block.php
@@ -1,43 +1,43 @@
-<div class="swiper">
-    <div class="swiper-wrapper header__features-list">
-        <div class="swiper-slide header__features-item">
-            <svg class="header__features-svg" width="36" height="36">
+<div class="swiper features">
+    <div class="swiper-wrapper features__slider-list">
+        <div class="swiper-slide features__slider-item">
+            <svg class="features__slider-svg" width="36" height="36">
                 <use href="./images/sprites.svg#1"></use>
             </svg>
-            <p class="header__features-text">Непрерывная работа c 2017 года</p>
+            <p class="features__slider-text">Непрерывная работа c 2017 года</p>
         </div>
-        <div class="swiper-slide header__features-item">
-            <svg class="header__features-svg" width="36" height="36">
+        <div class="swiper-slide features__slider-item">
+            <svg class="features__slider-svg" width="36" height="36">
                 <use href="./images/sprites.svg#2"></use>
             </svg>
-            <p class="header__features-text">Вся продукция сертифицирована</p>
+            <p class="features__slider-text">Вся продукция сертифицирована</p>
         </div>
-        <div class="swiper-slide header__features-item">
-            <svg class="header__features-svg" width="36" height="36">
+        <div class="swiper-slide features__slider-item">
+            <svg class="features__slider-svg" width="36" height="36">
                 <use href="./images/sprites.svg#3"></use>
             </svg>
-            <p class="header__features-text">Контроль качества на всех этапах</p>
+            <p class="features__slider-text">Контроль качества на всех этапах</p>
         </div>
-        <div class="swiper-slide header__features-item">
-            <svg class="header__features-svg" width="36" height="36">
+        <div class="swiper-slide features__slider-item">
+            <svg class="features__slider-svg" width="36" height="36">
                 <use href="./images/sprites.svg#4"></use>
             </svg>
-            <p class="header__features-text">Возможны поставки по всей России</p>
+            <p class="features__slider-text">Возможны поставки по всей России</p>
         </div>
-        <div class="swiper-slide header__features-item">
-            <svg class="header__features-svg" width="36" height="36">
+        <div class="swiper-slide features__slider-item">
+            <svg class="features__slider-svg" width="36" height="36">
                 <use href="./images/sprites.svg#5"></use>
             </svg>
-            <p class="header__features-text">Оперативное производство</p>
+            <p class="features__slider-text">Оперативное производство</p>
         </div>
     </div>
-    <div class="slider__buttons">
-        <div class="slider-button-prev">
+    <div class="slider__buttons features__slider-buttons">
+        <div class="slider-button-prev features__slider-buttons--prev">
             <svg width="36" height="24">
                 <use href="./images/sprites.svg#arrow-prev"></use>
             </svg>
         </div>
-        <div class="slider-button-next">
+        <div class="slider-button-next features__slider-buttons--next">
             <svg width="36" height="24">
                 <use href="./images/sprites.svg#arrow-next"></use>
             </svg>
