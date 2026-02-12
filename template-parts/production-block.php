9de155e9ed90499cddb521ae623f17b59c14b109
Filipp <wolverinexdd@yandex.ru>
2026-02-12 16:56:52 +0400

    fix cards on production-block

--- template-parts/production-block.php
+++ template-parts/production-block.php
@@ -9,58 +9,70 @@
         ?>
         <ul class="card__list">
             <li class="card__item card__item--product">
-                <div class="card__body">
-                    <h3 class="card__title">Автомобильная химия</h3>
-                    <p class="card__description">Безусловно, сплочённость команды профессионалов позволяет
-                        оценить
-                        значение форм воздействия.</p>
-                </div>
-                <img class="card__image" src="./images/cards-image/1.png" alt="Автомобильная химия" width="478"
-                    height="300" loading="lazy">
+                <a href="./autohimia.php" class="card__item-link">
+                    <div class="card__body">
+                        <h3 class="card__title">Автомобильная химия</h3>
+                        <p class="card__description">Безусловно, сплочённость команды профессионалов позволяет
+                            оценить
+                            значение форм воздействия.</p>
+                    </div>
+                    <img class="card__image" src="./images/cards-image/1.png" alt="Автомобильная химия" width="478"
+                        height="300" loading="lazy">
+                </a>
             </li>
             <li class="card__item card__item--product">
-                <div class="card__body">
-                    <h3 class="card__title">Бытовая химия</h3>
-                    <p class="card__description">А также стремящиеся вытеснить традиционное производство,
-                        нанотехнологии функционально разнесены на независимые элементы.</p>
-                </div>
-                <img class="card__image" src="./images/cards-image/2.png" alt="Бытовая химия" width="478" height="300"
-                    loading="lazy">
+                <a href="./autohimia.php" class="card__item-link">
+                    <div class="card__body">
+                        <h3 class="card__title">Бытовая химия</h3>
+                        <p class="card__description">А также стремящиеся вытеснить традиционное производство,
+                            нанотехнологии функционально разнесены на независимые элементы.</p>
+                    </div>
+                    <img class="card__image" src="./images/cards-image/2.png" alt="Бытовая химия" width="478"
+                        height="300" loading="lazy">
+                </a>
             </li>
             <li class="card__item card__item--product">
-                <div class="card__body">
-                    <h3 class="card__title">Дезинфицирующие средства</h3>
-                    <p class="card__description">Лишь интерактивные прототипы призваны к ответу.</p>
-                </div>
-                <img class="card__image" src="./images/cards-image/3.png" alt="Дезинфицирующие средства" width="478"
-                    height="300" loading="lazy">
+                <a href="./autohimia.php" class="card__item-link">
+                    <div class="card__body">
+                        <h3 class="card__title">Дезинфицирующие средства</h3>
+                        <p class="card__description">Лишь интерактивные прототипы призваны к ответу.</p>
+                    </div>
+                    <img class="card__image" src="./images/cards-image/3.png" alt="Дезинфицирующие средства" width="478"
+                        height="300" loading="lazy">
+                </a>
             </li>
             <li class="card__item card__item--product">
-                <div class="card__body">
-                    <h3 class="card__title">Пищевые аэрозоли</h3>
-                    <p class="card__description">Безусловно, сплочённость команды профессионалов позволяет
-                        оценить
-                        значение форм воздействия.</p>
-                </div>
-                <img class="card__image" src="./images/cards-image/3.png" alt="Пищевые аэрозоли" width="478"
-                    height="300" loading="lazy">
+                <a href="./autohimia.php" class="card__item-link">
+                    <div class="card__body">
+                        <h3 class="card__title">Пищевые аэрозоли</h3>
+                        <p class="card__description">Безусловно, сплочённость команды профессионалов позволяет
+                            оценить
+                            значение форм воздействия.</p>
+                    </div>
+                    <img class="card__image" src="./images/cards-image/3.png" alt="Пищевые аэрозоли" width="478"
+                        height="300" loading="lazy">
+                </a>
             </li>
             <li class="card__item card__item--product">
-                <div class="card__body">
-                    <h3 class="card__title">Косметическая продукция</h3>
-                    <p class="card__description">Лишь интерактивные прототипы призваны к ответу.</p>
-                </div>
-                <img class="card__image" src="./images/cards-image/4.png" alt="Косметическая продукция" width="478"
-                    height="300" loading="lazy">
+                <a href="./autohimia.php" class="card__item-link">
+                    <div class="card__body">
+                        <h3 class="card__title">Косметическая продукция</h3>
+                        <p class="card__description">Лишь интерактивные прототипы призваны к ответу.</p>
+                    </div>
+                    <img class="card__image" src="./images/cards-image/4.png" alt="Косметическая продукция" width="478"
+                        height="300" loading="lazy">
+                </a>
             </li>
             <li class="card__item card__item--product">
-                <div class="card__body">
-                    <h3 class="card__title">Краски аэрозольные</h3>
-                    <p class="card__description">А также стремящиеся вытеснить традиционное производство,
-                        нанотехнологии функционально разнесены на независимые элементы.</p>
-                </div>
-                <img class="card__image" src="./images/cards-image/5.png" alt="Краски аэрозольные" width="478"
-                    height="300" loading="lazy">
+                <a href="./autohimia.php" class="card__item-link">
+                    <div class="card__body">
+                        <h3 class="card__title">Краски аэрозольные</h3>
+                        <p class="card__description">А также стремящиеся вытеснить традиционное производство,
+                            нанотехнологии функционально разнесены на независимые элементы.</p>
+                    </div>
+                    <img class="card__image" src="./images/cards-image/5.png" alt="Краски аэрозольные" width="478"
+                        height="300" loading="lazy">
+                </a>
             </li>
         </ul>
     </div>
