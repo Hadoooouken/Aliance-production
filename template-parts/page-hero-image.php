52ad35f26178f60f3fef9fc0b2aa63bb28bdf84f
Filipp <wolverinexdd@yandex.ru>
2026-02-15 16:13:54 +0400

    fix image on hero

--- template-parts/page-hero-image.php
+++ template-parts/page-hero-image.php
@@ -5,10 +5,10 @@
                 <div class="hero__content hero__content--image">
                     <div class="separator"></div>
                     <h1 class="section__title hero__title <?= $page_title_color ?>"><?= $page_title ?></h1>
-                   <?php include_once('./template-parts/breadcrumb.php')?>
+                    <?php include_once('./template-parts/breadcrumb.php') ?>
+                </div>
                 <img class="hero__image" src="./images/hero/hero-avto-him.png" alt="" width="480" height="240"
                     loading="lazy">
             </div>
         </div>
-    </div>
 </section>
