7748f589868367f6ffda61645bbed0d43507d4c2
Filipp <wolverinexdd@yandex.ru>
2026-03-10 19:17:27 +0400

    added preload font

--- template-parts/head.php
+++ template-parts/head.php
@@ -4,12 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./fonts/fonts.css">
+    <link rel="preload" href="./fonts/BebasNeueRegular.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="./fonts/NotoSans-Regular.woff2" as="font" type="font/woff2" crossorigin>
+
+
     <link rel="stylesheet" href="./styles/swiper-bundle.min.css">
     <link rel="stylesheet" href="./styles/normalize.css">
+    <link rel="stylesheet" href="./fonts/fonts.css">
     <link rel="stylesheet" href="./styles/index.css">
+
     <script src="./src/swiper-bundle.min.js" defer></script>
     <script src="./src/just-validate.production.min.js" defer></script>
     <script src="./src/index.js" defer></script>
+
     <title><?= isset($page_title) ? $page_title . ' – Aliance Production' : 'Aliance Production' ?></title>
 </head>
