82c9cb25313982c908a62c2b695e5b700210fae8
Filipp <wolverinexdd@yandex.ru>
2026-03-11 14:07:48 +0400

    добавлена фавикона

--- template-parts/head.php
+++ template-parts/head.php
@@ -4,6 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="../images/favicons/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="../images/favicons/favicon.svg" />
+    <link rel="shortcut icon" href="../images/favicons/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="../images/favicons/apple-touch-icon.png" />
     <link rel="preload" href="./fonts/BebasNeueRegular.woff2" as="font" type="font/woff2" crossorigin>
     <link rel="preload" href="./fonts/NotoSans-Regular.woff2" as="font" type="font/woff2" crossorigin>
     <link rel="preload" href="./fonts/NotoSans-SemiBold.woff2" as="font" type="font/woff2" crossorigin>
